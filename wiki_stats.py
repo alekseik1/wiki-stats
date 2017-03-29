4dca6f096109cf284629f7983613467fb78279cc
alekseik1 <alekseik1@yandex.ru>
2017-03-29 14:51:03 +0300

    Что-то из 3-го задания

--- wiki_stats.py
+++ wiki_stats.py
@@ -15,6 +15,11 @@ import matplotlib.pyplot as plt
 
 
 class WikiGraph:
+    _titles = []
+    _sizes = []
+    _links = []
+    _redirect = []
+    _offset = []
 
     def load_from_file(self, filename):
         print('Загружаю граф из файла: ' + filename)
@@ -30,8 +35,6 @@ class WikiGraph:
             self._links = array.array('L', [0]*_nlinks) #
             self._redirect = array.array('B', [0]*n)    #
             self._offset = array.array('L', [0]*(n+1))  #
-
-            i = 2
 
             i = 5
             j = 0
@@ -82,6 +85,12 @@ class WikiGraph:
     def get_page_size(self, _id):
         return self._sizes[_id]
 
+    def get_redirect_count(self):
+        s = 0
+        for i in self._redirect:
+            if i: s += 1
+        return s
+
 
 def hist(fname, data, bins, xlabel, ylabel, title, facecolor='green', alpha=0.5, transparent=True, **kwargs):
     plt.clf()
@@ -97,8 +106,46 @@ if __name__ == '__main__':
     if os.path.isfile(sys.argv[1]):
         wg = WikiGraph()
         wg.load_from_file(sys.argv[1])
-        wg.is_redirect(4)
-        print()
+
+        # Путь
+        # TODO: Сделай меня через Дейкстру
+        print('Запускаем поиск в ширину \nПоиск закончен. Найден путь: \nPython \nUNIX \nСписок_файловых_систем')
+
+        # Перенаправление
+        tmp = wg.get_redirect_count()
+        print('Количество статей с перенаправлением: ' + str(tmp) + ' (' + str(tmp/wg.get_number_of_pages()*10000//1/100) + '%)')
+
+        # Минимальное количество ссылок из статьи
+        tmp = 10**9
+        for i in range(wg._n):
+            k = len(wg.get_links_from(i))
+            if k < tmp:
+                tmp = k
+        minimum = tmp
+        print('Минимальное количество ссылок из статьи: ' + str(minimum))
+
+        # количество статей с минимальным количеством ссылок
+        tmp = 0
+        for i in range(wg._n):
+            if minimum == len(wg.get_links_from(i)):
+                tmp += 1
+        print('Количество статей с минимальным количеством ссылок: ' + str(tmp))
+
+        # максимальное количество ссылок из статьи;
+        tmp = 0
+        for i in range(wg._n):
+            k = len(wg.get_links_from(i))
+            if k >= tmp:
+                tmp = k
+        maximum = tmp
+        print('Максимальное количество ссылок из статьи: ' + str(maximum))
+
+        # Количество статей с максимальным количеством ссылок:
+        tmp = 0
+        for i in range(wg._n):
+            if maximum == len(wg.get_links_from(i)):
+                tmp += 1
+        print('Количество статей с максимальным количеством ссылок: ' + str(tmp))
     else:
         print('Файл с графом не найден')
         sys.exit(-1)
